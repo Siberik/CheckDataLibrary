81de1cee5152cb9271e5edc58457ef91493cd656
lolga <lolga@VLADI>
2022-12-27 15:36:05 +0300

    Добавьте файлы проекта.

---
+++ CheckDataLibrary/CheckDataClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDataLibrary
+{
+    public class CheckDataClass
+    {
+        public bool CheckVin(string vin, int year)
+        {
+        
+        }
+    }
+}
