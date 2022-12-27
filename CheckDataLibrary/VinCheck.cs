11f94849ec6af3943c00ac712caa7dc16e25206b
Влуд Губарев <lolgagr@gmail.com>
2022-12-27 17:14:05 +0300

    Сделаны первичные тесты, проверяющие, подходит ли программа первичным значениям

---
+++ CheckDataLibrary/VinCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckDataLibrary
+{
+    public class VinCheck
+    {
+    /// <summary>
+    /// Проверка на правильность введённого VIN и даты
+    /// </summary>
+    /// <param name="vin">
+    /// сам VIN автомобиля
+    /// </param>
+    /// <param name="year">
+    /// Год производства
+    /// </param>
+    /// <returns>
+    /// Возвращает истину или ложь
+    /// </returns>
+        public bool CheckVin(string vin, int year)
+        {
+            if (vin == string.Empty || vin.Length != 17 || String.IsNullOrWhiteSpace(vin) || 1980 > year || year>Convert.ToInt32(DateTime.Now.Year))
+            {
+                return false;
+            }
+            if (!vin.All(char.IsLetterOrDigit)|| vin.ToUpper().Contains('I') || vin.ToUpper().Contains('O') || vin.ToUpper().Contains('Q'))
+            {
+                return false;
+            }
+            var vinSplit = vin.Split(' ').ToArray();
+            for (int i = 0; i > vinSplit.Length; i++)
+            {
+                
+
+            }
+            return true;
+        }
+    }
+}
---
+++ CheckDataLibraryTests/VinCheckTest.cs
@@ -0,0 +1,147 @@
+using CheckDataLibrary;
+namespace CheckDataLibraryTests
+   
+{
+    [TestClass]
+    public class VinCheckTest
+    {
+        /// <summary>
+        /// Проверка VIN на пустую строку
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_StringEmpty_ReturnedFalse()
+        {
+            //Arrange
+            string vin=String.Empty;
+            int year = 2000;
+            //Act
+           VinCheck check= new VinCheck();
+            bool actual=check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на 17 пробелов
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_17Space_ReturnedFalse()
+        {
+            //Arrange
+            string vin ="                 ";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на маленький VIN
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_SmallLenght_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "123";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка маленький год
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_PrevYear_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C404453";
+            int year = 1919;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на все соответствующие условия
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_RightVin_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C404453";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsTrue(actual);
+        }
+        /// <summary>
+        /// Проверка на большой год
+        /// </summary>
+        [TestMethod]
+        public void CheckVin_NextYear_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C404453";
+            int year = 2077;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void CheckVin_SpecialSymbols_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C40445+";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void CheckVin_ForbiddenSymbolQ_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C40445Q";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void CheckVin_ForbiddenSymbolI_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C40445I";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod]
+        public void CheckVin_ForbiddenSymbolO_ReturnedFalse()
+        {
+            //Arrange
+            string vin = "JHMCM56557C40445O";
+            int year = 2000;
+            //Act
+            VinCheck check = new VinCheck();
+            bool actual = check.CheckVin(vin, year);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}
